f7d3723a024615a0f3f14860340687e2a0d82bcf
Sonali Agarwal <agarwalsonali922@gmail.com>
2025-12-21 14:09:41 +0530

    Fix MySQL ECONNRESET using pool

--- backend/db/index.js
+++ backend/db/index.js
@@ -3,25 +3,29 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
   connectTimeout: 20000
 });
 
 /* ================= CONNECT ================= */
 
-db.connect((err) => {
+db.getConnection((err, connection) => {
   if (err) {
-    console.error("❌ Database connection failed:", err.message);
-    return;
+    console.error("❌ MySQL Pool Connection Failed:", err);
+  } else {
+    console.log("✅ MySQL Pool Connected");
+    connection.release();
   }
-  console.log("✅ Connected to MYSQL Database");
-
 });
+
 
 // /* ================= USERS TABLE ================= */
 
